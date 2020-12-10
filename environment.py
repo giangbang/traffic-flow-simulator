5b983d9b47392b61cd8251934b947ba5a1770e4d
giangbang <banggiangle2015@gmail.com>
2020-12-10 20:47:48 +0700

    finish

---
+++ environment.py
@@ -0,0 +1,54 @@
+import traci
+import numpy as np
+
+class environment():
+    def __init__(self):
+    
+        self.junctions = {}
+        self.programLightDict = {}
+        trafficlights = list(traci.trafficlight.getIDList())
+        for trafficlight in trafficlights:
+            lanes = set(traci.trafficlight.getControlledLanes(trafficlight))
+            self.junctions[trafficlight] = lanes
+            tmp = list(traci.trafficlight.getCompleteRedYellowGreenDefinition(trafficlight)[0].phases)
+            self.programLightDict[trafficlight] = [phase.state for phase in tmp]
+        print(self.programLightDict)
+        print(self.junctions)
+        
+    def getWaitingTime(self):
+        res = 0
+        for i, junc in enumerate(self.junctions): 
+            for lane in self.junctions[junc]:
+                res += (traci.lane.getWaitingTime(lane))
+        return res
+        
+    def reward(self):
+        return 2.5-np.array([self.getWaitingTime()]).reshape(1, 1) / 2e3
+        
+    def getPhase(self):
+        return (traci.trafficlight.getPhase('0'))
+           
+    def getState(self):
+        state = np.zeros(0)
+        for junc in self.junctions:
+            numLanes = len(self.junctions[junc])
+            numPhases = len(self.programLightDict[junc])
+            state = np.zeros((numLanes*2 + numPhases), dtype=np.float32)
+            for i, id in enumerate(self.junctions[junc]):
+                state[i*2] = traci.lane.getLastStepOccupancy(id)
+                state[i*2+1] = traci.lane.getLastStepMeanSpeed(id)
+        phase = self.getPhase()
+        state[numLanes*2 + phase] = 1
+        return state[np.newaxis, ...]
+        
+    def state_size(self):
+        return self.getState().flatten().shape[0]
+        
+    def action_size(self):
+        return len(self.programLightDict['0'])
+    
+    def do_action(self, phaseid):
+        for i, junc in enumerate(self.junctions):
+            traci.trafficlight.setPhase(junc, phaseid[i])
+            
+        return self.getState(), self.reward()
--- test.py
+++ test.py
@@ -3,6 +3,9 @@
 import os
 import sys
 import optparse
+import time
+from environment import environment
+# from agent import agent
 
 # we need to import some python modules from the $SUMO_HOME/tools directory
 if 'SUMO_HOME' in os.environ:
@@ -21,26 +24,16 @@ def get_options():
     opt_parser = optparse.OptionParser()
     opt_parser.add_option("--nogui", action="store_true",
                          default=False, help="run the commandline version of sumo")
+    opt_parser.add_option("--train", action="store_true",
+                         default=False, help="run the commandline training of agent")
     options, args = opt_parser.parse_args()
     return options
 
 
 # contains TraCI control loop
-def run():
-    step = 0
-    while traci.simulation.getMinExpectedNumber() > 0:
-        traci.simulationStep()
-        step += 1
-    
-    print(step)
-    traci.close()
-    sys.stdout.flush()
-
-
-# main entry point
-if __name__ == "__main__":
+def run(env, episode):
     options = get_options()
-
+    
     # check binary
     if options.nogui:
         sumoBinary = checkBinary('sumo')
@@ -50,4 +43,26 @@ if __name__ == "__main__":
     # traci starts sumo as a subprocess and then this script connects and runs
     traci.start([sumoBinary, "-c", "test.sumocfg",
                              "--tripinfo-output", "tripinfo.xml"])
-    run()
+                             
+    env = environment()
+    # agt = agent()
+    
+
+    while episode > 0:
+        episode -= 1
+        traci.start([sumoBinary, "-c", "test.sumocfg",
+                             "--tripinfo-output", "tripinfo.xml"])
+        while traci.simulation.getMinExpectedNumber() > 0:
+            traci.simulationStep()
+            print(env.state_size())
+            print(env.action_size())
+            print(env.reward())
+            break
+  
+    traci.close()
+    sys.stdout.flush()
+
+
+# main entry point
+if __name__ == "__main__":
+    run(1)
